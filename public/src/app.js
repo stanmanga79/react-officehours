8c0586aec9c9ee79166388dd59c5d355390f6571
Stanley Lewis <stanmanga79@gmail.com>
2023-11-21 18:50:26 -0500

    Add Display for profiles

--- public/src/app.js
+++ public/src/app.js
@@ -2,6 +2,17 @@ let profileListEL = document.getElementById("profile-list")
 let btnProfile = document.getElementById("btnProfile")
 let profileNameEL = document.getElementById("profile-name");
 
-const addDisplay = () => {
-
+const addDisplay = (name, endorsedCount) => {
+    profileListEL.innerHTML += `
+    <div class="col-12 col-xl-6">
+        <div class="card mb-3">
+            <h4 class="card-header bg-dark text-light p-2 m-0">${name} <br><span class="text-white"
+                    style="font-size: 1rem;">currently has ${endorsedCount} endorsed
+                    skills</span></h4><a class="btn btn-block btn-squared btn-light text-dark"
+                href="/profiles/655c1322f5b34b60b39db3d1">View and endorse their
+                skills.</a>
+        </div>
+    </div>
+    
+    `
 } 
